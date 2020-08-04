52612e6736303a07bc10bdb7014071508812d870
BukkyOmo <odunayobukky1@gmail.com>
2020-08-04 10:01:19 +0100

    chore-set-up-express-application

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to Wonder!!!',
+        status: 'Success',
+        statusCode: 200
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
